bf7c19be1941d22b8f4a360034d45535f8d1c419
sgg2123 <sgg2123@columbia.edu>
2018-04-19 23:39:59 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(num)
   if num%3==0
-    retrun Fizz
+    retrun "Fizz"
   elsif num%5==0
-    return Buzz
+    return "Buzz"
   elsif num%3 ==0 && num%5==0
-    return FizzBuzz
+    return "FizzBuzz"
   else 
     return 
   end
